2e032eee86fd7452f81aa6da05bf87155278ae5f
Aparna Shaju <aparnakanatil2002@gmail.com>
2026-02-26 16:47:57 +0530

    middleware changed

--- Backend/src/middleware/authMiddleware.js
+++ Backend/src/middleware/authMiddleware.js
@@ -1,13 +1,21 @@
-const jwt = require('jsonwebtoken')
-module.exports=function(req,res,next){
-    const token=req.header("Authorization")
-    if(!token) return res.status(401).json({message:"No token, access denied"})
-    try{
-        const verified=jwt.verify(token,process.env.JWTSECRET)
-        req.user=verified
-        next()
-    }catch(err){
-        res.status(400).json({message:"Invalid token"})
-    }
-    
-}
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token, access denied" });
+  }
+
+  const token = authHeader.startsWith("Bearer ")
+    ? authHeader.split(" ")[1]
+    : authHeader;
+
+  try {
+    const verified = jwt.verify(token, process.env.JWTSECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
