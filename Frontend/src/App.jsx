d9b77f4b74af105ced4b6b4ed781da1eea8c49af
Aparna Shaju <aparnakanatil2002@gmail.com>
2026-02-26 16:50:53 +0530

    the protect routed added in the app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Dashboard from './components/Dashboard'
+import KYC from './components/KycPage'
+import ProtectedKYC from './ProtectRoute/ProtectedKyc'
+import ProtectedRoute from './ProtectRoute/ProtectedRoute'
 
 function App() {
 
@@ -13,7 +16,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Login/>} />
           <Route path='/signup' element={<Signup/>} />
-          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/kyc' element={<ProtectedKYC><KYC/></ProtectedKYC>} />
+          <Route path='/dashboard' element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
         </Routes>
       </Router>
     </>
