8bf8e60d66c8c738da761e1065906a78b2f09b97
Aparna Shaju <aparnakanatil2002@gmail.com>
2026-03-05 15:09:42 +0530

    userSchema,userModal and db connection added

---
+++ Backend/src/framework/database/schema/userSchema.ts
@@ -0,0 +1,33 @@
+import { Schema } from "mongoose"
+
+export const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 6
+    },
+
+    imageUrl: {
+      type: String,
+      default: null
+    },
+
+    audioUrl: {
+      type: String,
+      default: null
+    }
+
+  },
+  {
+    timestamps: true
+  }
+)
